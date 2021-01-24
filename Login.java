81f8591e26087523ff8103619868cf5ecacd6c3b
TirthaB <tirthankar.personal@hotmail.com>
2021-01-24 05:58:08 -0800

    Login functionality implemented

--- Login.java
+++ Login.java
@@ -1,5 +1,13 @@
 class Login{
 	public static void main(String args[]){
 		System.out.println("Login Functionality has to be developed");
+		String username = "admin";
+		String password = "admin";
+		verifyLogin(username,password);
 	}
+	
+	public boolean verifyLogin(String username, String password){
+	if(username.equals("admin")&& password.equals("admin")){
+		return true;
+	}else { return false;}
 }
